569ce07b2bd7d6a403c582b6be663078968bb543
Ihor Fostiak <i.fostiak@goit.ua>
2022-01-23 18:52:58 +0200

    init

---
+++ NameParserTest.java
@@ -0,0 +1,18 @@
+class NameParser {
+    public String parse(String[] names) {
+        int count = names.length;
+        int cnt = 0;
+        StringBuilder preResult = new StringBuilder();
+        for (String oneName : names
+        ) {
+            preResult.append(oneName.split(" ")[0]);
+            if (cnt < count - 1) {
+                preResult.append(", ");
+            }
+            cnt++;
+
+        }
+        return preResult.toString();
+
+    }
+}
